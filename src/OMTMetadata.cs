12f0b2ffdf11ff41014ba97d35638b3d777f1f3b
vmixmartin <111566885+vmixmartin@users.noreply.github.com>
2026-09-07 00:54:50 +1000

    Revert "Fix invalid TALLY XMLs"

--- src/OMTMetadata.cs
+++ src/OMTMetadata.cs
@@ -40,18 +40,10 @@ namespace libomtnet
         public const string CHANNEL_SUBSCRIBE_METADATA = @"<OMTSubscribe Metadata=""true"" />";
         public const string CHANNEL_PREVIEW_VIDEO_ON = @"<OMTSettings Preview=""true"" />";
         public const string CHANNEL_PREVIEW_VIDEO_OFF = @"<OMTSettings Preview=""false"" />";
-        public const string TALLY_PREVIEW = @"<OMTTally Preview=""true"" Program=""false"" />";
-        public const string TALLY_PROGRAM = @"<OMTTally Preview=""false"" Program=""true"" />";
-        public const string TALLY_PREVIEWPROGRAM = @"<OMTTally Preview=""true"" Program=""true"" />";
-        public const string TALLY_NONE = @"<OMTTally Preview=""false"" Program=""false"" />";
-        // Legacy tally XML. A historical bug wrote Program== instead of Program=.
-        // Metadata commands are matched as exact strings for performance and are not parsed
-        // as XML, so devices and earlier library versions that still send these tokens would
-        // otherwise be ignored. Receivers must therefore accept both forms. See issue #51.
-        public const string TALLY_PREVIEW_LEGACY = @"<OMTTally Preview=""true"" Program==""false"" />";
-        public const string TALLY_PROGRAM_LEGACY = @"<OMTTally Preview=""false"" Program==""true"" />";
-        public const string TALLY_PREVIEWPROGRAM_LEGACY = @"<OMTTally Preview=""true"" Program==""true"" />";
-        public const string TALLY_NONE_LEGACY = @"<OMTTally Preview=""false"" Program==""false"" />";
+        public const string TALLY_PREVIEW = @"<OMTTally Preview=""true"" Program==""false"" />";
+        public const string TALLY_PROGRAM = @"<OMTTally Preview=""false"" Program==""true"" />";
+        public const string TALLY_PREVIEWPROGRAM = @"<OMTTally Preview=""true"" Program==""true"" />";
+        public const string TALLY_NONE = @"<OMTTally Preview=""false"" Program==""false"" />";
     }
     internal class OMTMetadataTemplates
     {
